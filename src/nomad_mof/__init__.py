de089560179e4b4b1c9f3d3df3987e8333dddc7a
Hampus Näsström <hampus.nasstrom@gmail.com>
2024-01-23 11:40:27 +0100

    Added plugin skeleton

---
+++ src/nomad_mof/__init__.py
@@ -0,0 +1,2 @@
+from nomad_mof.schema import *
+from nomad_mof.parser import *
---
+++ src/nomad_mof/schema.py
@@ -0,0 +1,12 @@
+from nomad.metainfo import (
+    Section,
+)
+
+from nomad.datamodel.data import (
+    EntryData,
+)
+
+class MOF(EntryData):
+    m_def = Section(
+        a_template={}
+    )
